eb8ea798c655437da79f18a970f71f81a5b56d3a
FlorSalcedoWD <florenciasalcedo13@gmail.com>
2020-02-05 16:36:22 -0300

    Limpieza de código II

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -22,10 +22,8 @@ export class NavbarComponent implements OnInit {
   getCurrentUser() {
     this.AuthService.isAuth().subscribe( auth => {
       if (auth) {
-        console.log('user logged');
         this.isLogged = true;
       } else {
-        console.log('NOT user logged');
         this.isLogged = false;
       }
     });
